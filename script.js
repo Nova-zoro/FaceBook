0a180bf7b31ffddd7c153d520c9f7f44dee6ba8e
Vivek Nova <110530286+Nova-zoro@users.noreply.github.com>
2022-11-23 19:46:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuItem =  document.querySelectorAll(".menu-item");
+
+menuItem.forEach(item => {
+    item.addEventListener("click" , () => {
+        item.classList.add("active");
+    })
+}) 
+

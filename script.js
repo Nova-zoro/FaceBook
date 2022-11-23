807d489afc0472ca54d723ab36c66147dfcba3ef
Vivek Nova <110530286+Nova-zoro@users.noreply.github.com>
2022-11-23 19:48:47 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuItem =  document.querySelectorAll(".menu-item");
-
-menuItem.forEach(item => {
-    item.addEventListener("click" , () => {
-        item.classList.add("active");
-    })
-}) 
-
